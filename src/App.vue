df49187e0dfb724f7d3f2b0a9cc1f2cc41d6ba16
federico_viganò <fede_viga@hotmail.it>
2024-03-04 15:22:01 +0100

    creata navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <script>
 import axios from 'axios';
+import AppNav from './components/AppNav.vue';
 
 export default {
 
   data() {
     return {
-
+      cards: [],
     }
   },
 
-  components: {
+  created() {
 
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(result => {
+      console.log(result.data.data)
+      this.cards = result.data.data;
+    })
+  },
+
+  components: {
+    AppNav,
   },
 }
-
 </script>
 
 <template>
-  
+
+  <AppNav></AppNav>
+
 </template>
 
 <style lang="scss">
---
+++ src/components/AppNav.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'AppNav',
+}
+</script>
+
+<template>
+
+<nav>
+    <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="logo Yu-Gi-Oh">
+    <h1>
+        Yu-Gi-Oh Api
+    </h1>
+</nav>
+
+</template>
+
+<style lang="scss">
+ nav {
+    display: flex;
+    align-items: center;
+    background-color: white;
+    color: black;
+    padding: 24px 36px;
+    gap: 36px;
+
+    img {
+        height: 34px;
+    }
+
+    h1 {
+        font-size: 40px;
+        font-weight: 400;
+    }
+ }
+</style>
